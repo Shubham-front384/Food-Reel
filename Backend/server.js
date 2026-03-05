1b118f2639f71042ab3a3961f446ddaa6fcb6172
Shubham-front384 <shubhamworkemail07@gmail.com>
2026-03-05 18:48:49 +0530

    Done

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,10 @@
 require("dotenv").config();
 const app = require("./src/app");
 const connectToDb = require("./src/config/database");
+const PORT = 3000;
 
 connectToDb();
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port: ${process.env.PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port: ${PORT}`);
 });
--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,15 +1,12 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
-const cors = require("cors");
+const path = require("path")
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true
-}));
+app.use(express.static("./public"));
 
 // Router require
 const authRouter = require("./routes/auth.route");
@@ -18,5 +15,9 @@ const foodRouter = require("./routes/food.route");
 // Router use
 app.use("/api/auth", authRouter);
 app.use("/api/food", foodRouter);
+
+app.use("*name", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"))
+})
 
 module.exports = app;
